eeb0391a5b66cc7ffa771c2668f659699b8b9534
jpt <redacted@gmail.com>
2025-11-19 21:18:24 -0500

    Initial Commit

---
+++ Rugburn/RugburnApp.swift
@@ -0,0 +1,17 @@
+//
+//  RugburnApp.swift
+//  Rugburn
+//
+//  Created by J T on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct RugburnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RugburnTests/RugburnTests.swift
@@ -0,0 +1,16 @@
+//
+//  RugburnTests.swift
+//  RugburnTests
+//
+//  Created by J T on 11/19/25.
+//
+
+import Testing
+
+struct RugburnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
